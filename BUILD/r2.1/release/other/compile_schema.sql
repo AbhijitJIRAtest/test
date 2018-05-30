0a19fb76f69bb87d7f2ff3f236862c48188af0db
AbhijitDebnath1 <33083157+AbhijitDebnath1@users.noreply.github.com>
2018-05-30 19:41:32 +0530

    Delete compile_schema.sql

--- BUILD/r2.1/release/other/compile_schema.sql
+++
@@ -1,8 +0,0 @@
-BEGIN
-  FOR i IN (SELECT du.username 
-            FROM DBA_USERS du 
-            WHERE du.username LIKE 'DB%ADMIN' OR du.username LIKE '%PORTAL') LOOP
-      DBMS_UTILITY.COMPILE_SCHEMA(i.username);
-  END LOOP;
-END;
-/
